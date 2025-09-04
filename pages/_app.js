61d7da9a729d91568086f7edb4ee8c061fd730b7
ivanhoffman <ivan@optimion.us>
2025-09-04 14:43:21 -0400

    GTAG Update

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,8 @@ import Script from "next/script";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 
-const GA_MEASUREMENT_ID = "G-GWBX9F3PLV";
+// Use env var if present, fall back to your GTM container ID
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-PQJFJPTW";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -15,9 +16,7 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     const scrollToHash = (url) => {
       const candidate =
-        typeof url === "string"
-          ? url
-          : window.location.pathname + window.location.hash;
+        typeof url === "string" ? url : window.location.pathname + window.location.hash;
       const hash = candidate.split("#")[1];
       if (!hash) return;
 
@@ -29,21 +28,21 @@ export default function App({ Component, pageProps }) {
       });
     };
 
-    // Initial load (covers direct hit to /#faq)
-    scrollToHash(router.asPath);
-
-    // After client-side route changes
+    scrollToHash(router.asPath); // initial load
     router.events.on("routeChangeComplete", scrollToHash);
     return () => router.events.off("routeChangeComplete", scrollToHash);
   }, [router.events, router.asPath]);
 
-  // GA4: send page_view on client-side route changes
+  // Push virtual pageviews for SPA navigations to GTM
   useEffect(() => {
     const handleRouteChange = (url) => {
-      if (typeof window.gtag === "function") {
-        window.gtag("config", GA_MEASUREMENT_ID, { page_path: url });
-      } else if (window.dataLayer) {
-        window.dataLayer.push({ event: "page_view", page_path: url });
+      if (window.dataLayer) {
+        window.dataLayer.push({
+          event: "page_view",
+          page_path: url,
+          page_location: window.location.href,
+          page_title: document.title,
+        });
       }
     };
     router.events.on("routeChangeComplete", handleRouteChange);
@@ -52,18 +51,14 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      {/* Google tag (gtag.js) */}
-      <Script
-        id="gtag-src"
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
-      <Script id="gtag-config" strategy="afterInteractive">
+      {/* Google Tag Manager */}
+      <Script id="gtm-init" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
         `}
       </Script>
 
--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-PQJFJPTW";
+
   return (
     <Html lang="en">
       <Head>
@@ -9,7 +11,7 @@ export default function Document() {
         <link rel="preconnect" href="https://cal.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://app.cal.com" crossOrigin="anonymous" />
 
-        {/* Favicon & app icons (files should be in /public) */}
+        {/* Favicon & app icons */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -18,6 +20,16 @@ export default function Document() {
         <meta name="theme-color" content="#000000" />
       </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
